af004897d45c6ce014215eaf62a458c054e79791
tusharpatwal001 <thinkdifferent782@gmail.com>
2025-10-05 00:15:17 +0530

    leet code problem 217

--- leet-code/q7.py
+++ leet-code/q7.py
@@ -1,10 +1,19 @@
-from typing 
-def containsDuplicate(self, nums: List[int]) -> bool:
-        ls1 = []
-        for i in nums:
-            if nums[i] in ls1:
-                return True
-            else:
-                print(ls1)
-                ls1.append(nums[i])
-        return False
+# 217. Contains Duplicate
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+from typing import List
+
+
+def containsDuplicate(nums: List[int]) -> bool:
+    ls1 = []
+    for i in range(len(nums)):
+        if nums[i] in ls1:
+            return True
+        else:
+            ls1.append(nums[i])
+            print(ls1)
+    return False
+
+
+print(containsDuplicate([1, 2, 3, 4]))
