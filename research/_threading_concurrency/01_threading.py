8c1a391979738580b798c6ad91e420b4f8b34ebf
tusharpatwal001 <thinkdifferent782@gmail.com>
2025-11-30 20:51:36 +0530

    threading examples

---
+++ research/_threading_concurrency/01_threading.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+
+def take_orders():
+    for i in range(1, 4):
+        print(f"Taking order for #{i}")
+        time.sleep(2)
+
+
+def brew_chai():
+    for i in range(1, 4):
+        print(f"Brewing chai for #{i}")
+        time.sleep(3)
+
+
+# creating threads
+order_thread = threading.Thread(target=take_orders)
+brew_thread = threading.Thread(target=brew_chai)
+
+order_thread.start()
+brew_thread.start()
+
+# wait for both to finish
+order_thread.join()
+brew_thread.join()
+
+print(f"All orders taken and chau brewed")
