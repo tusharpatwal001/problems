f6b8d1965fb46d93056a6987846ca7b60a05754f
tusharpatwal001 <thinkdifferent782@gmail.com>
2025-12-25 13:49:58 +0530

    cleaner for report scope

---
+++ research/rud_test_case/report_scope_cleaner.py
@@ -0,0 +1,24 @@
+with open("jsons/report_scope.json", encoding='utf-8')as f:
+    import json
+    data = json.load(f)
+
+all_keys = [key for key in list(data.keys())]
+
+cleaned_dict = dict()
+
+for item in [{i: data[i]} for i in all_keys]:
+    for key, value in item.items():
+        for v in list(value.keys()):
+            if v != "By Region":
+                first_half = ", ".join(value[v][:-1]).strip()
+                second_half = value[v][-1].replace(" and", ",")
+
+                new_str = first_half + ", " + second_half
+                new_value = new_str.split(", ")
+                if "" in new_value:
+                    new_value.remove("")
+                value[v] = new_value
+        cleaned_dict[key] = value
+
+with open("jsons/cleaned_report_scope.json", "w", encoding='utf-8') as f:
+    json.dump(cleaned_dict, f, ensure_ascii=False)
